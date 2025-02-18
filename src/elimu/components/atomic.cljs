65e56e6a45bbd23564d02c3cb015a3654c874b74
Andre Borba <dev_andre.filipe@outlook.com>
2025-02-17 22:42:52 -0300

    refactor: Remove atomic components and home page implementation

--- src/elimu/components/atomic.cljs
+++
@@ -1,7 +0,0 @@
-(ns elimu.components.atomic)
-
-(defn alert-button []
-  [:button {:type "button"
-            :class "alert-button alert-button-background"
-            :onClick #(js/alert "Hello, world!")
-            :href "https://github.com/af2b"} "Click me!"])
--- src/elimu/pages/home.cljs
+++
@@ -1,7 +0,0 @@
-(ns elimu.pages.home
-  (:require [elimu.components.atomic :as atomic]))
-
-(defn home-page []
-  [:div [:h2 "Bem vindo ao Elimu!"]
-   [:p "Clique no botão abaixo para ver um alerta."]
-   [atomic/alert-button]])
