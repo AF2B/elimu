2e4173dc8b5d60ecbdce0e050f55bb7894e3ee1d
Andre Borba <dev_andre.filipe@outlook.com>
2025-02-20 21:46:58 -0300

    feat: Integrate carousel component into the main app view

--- src/elimu/core.cljs
+++ src/elimu/core.cljs
@@ -1,11 +1,13 @@
 (ns elimu.core
   (:require [reagent.core :as r]
             [reagent.dom :as d]
-            [elimu.components.header.header :refer [header]]))
+            [elimu.components.header.header :refer [header]]
+            [elimu.components.carousel.carousel :refer [carousel]]))
 
 (defn app []
   [:div
-   [header]])
+   [header]
+   [carousel]])
 
 (defn mount-root []
   (d/render [app] (.getElementById js/document "app")))
